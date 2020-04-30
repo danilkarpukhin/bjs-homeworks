326f21f6f03dc7c4b4bcfbabe5da19209737a117
DanilKarpukhin <danilkarpukhin@ya.ru>
2020-04-30 14:45:48 +0600

    danil

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,28 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    let totalAmount, credit, totalMonth, payment;
+    if (isNaN (parseInt (percent))) {
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    } else if (isNaN(parseInt (contribution))) {
+        return `Параметр "Начальный взонс" содержит неправильное значение ${contribution}`;
+    } else if (isNaN(parseInt(amount))) {
+		return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+	} else if (new Date > date) {
+		return `Параметр дата введен не верно`;
+    };
+    
+    percent = percent / 12 / 100;
+	credit = amount - contribution;
+	totalMonth = ((date.getFullYear() - new Date().getFullYear()) * 12) + (date.getMonth() - new Date().getMonth()); 
+	payment = credit * (percent + percent / (Math.pow(1+percent, totalMonth)-1));
+	return totalAmount = Number((payment * totalMonth).toFixed(2));
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    if  ((name == "") || (name == null) || (!name == undefined)) {
+        return `Привет, мир! Меня зовут Аноним`;
+ };
+     return greeting = `Привет, мир! Меня зовут ${name}`;
 }
