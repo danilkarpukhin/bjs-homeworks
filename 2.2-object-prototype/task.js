98a80f31bb01f6e12ecb861615e849ca8b9e8fd8
DanilKarpukhin <danilkarpukhin@ya.ru>
2020-05-08 11:56:26 +0600

    2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,35 @@
 function getAnimalSound(animal) {
     // код для задачи №1 писать здесь
-    // return animalSound;
+    let animalSound;
+    if (animal == undefined) {
+        animalSound = null;
+    } else {
+        animalSound = animal.sound;
+    }
+     return animalSound;
 }
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    let total = 0;
+    if (marks.length == 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < marks.length; i ++) {
+            total = total + Number(marks[i]);
+        }
+    }
+    let average = total / marks.length;
+    let averageMark = Math.round(average);
+     return averageMark;
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    // return verdict
+    const now = new Date().getTime();  
+  const birthdays = new Date(birthday).getTime();  
+  const age = now - birthdays;
+  const diff = age / (1000 * 24 * 3600 * 366);
+  let verdict = (age > 18) ? true : false;
+  return verdict;
 }
