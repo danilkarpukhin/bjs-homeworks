544220946aa1e478b73706762fb1310515ab620c
DanilKarpukhin <danilkarpukhin@ya.ru>
2020-05-17 15:53:26 +0600

    verson2

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -15,9 +15,7 @@ function sum(...args) {
 function compareArrays (arr1, arr2) {
   if (Array.isArray(arr1) && Array.isArray(arr2)){
     if (arr1.length == arr2.length) {
-      return arr1.every( (number, index) => {
-        return number == arr2[index];
-      });
+      return arr1.length == arr2.length && arr1.every((number, index) => number == arr2[index]);
     } else {
     return false;
     }
@@ -26,21 +24,30 @@ function compareArrays (arr1, arr2) {
   }
 }
 
-function memorize (fn, limit) {
-  let memory = new Array(limit);
-  let auxiliaryIndex;
-  return ((...args) => {
-    if (memory.some( (number, index) => { auxiliaryIndex = index; return compareArrays(number.arr, args); })) {
-      return memory[auxiliaryIndex].result;
-    } else {
-      let res = fn(...args);
-      memory.pop();
-      memory.unshift({arr: args, result: res});
-      return res;
-    }
-  });
 
-}
+  function memorize(fn, limit) {
+    const memory = [];
+  
+    return function (...args) {
+      let find = memory.find(element => compareArrays(element.args, args));
+  
+    if (find) {
+      console.log('Вызов из памяти');
+      return find.result;
+    }
+    
+    let result = fn(...args);
+  
+     if (memory.length > limit) {
+       memory.shift();
+       memory.push({args, result});
+      } else {      
+        memory.push({args, result})            
+      }
+      return result;
+    }
+  }
+  
 
 let timeTestArr = [[1, 2, 3], [2, 5], [1, 2, 3], [2, 5], [7, 8, 13, 1], [2, 5]];
 let mSum = memorize(sum, 5);
@@ -50,7 +57,7 @@ let label = 'abs';
 
 function testCase (func) {
   console.time(label);
-  for (var i = 0; i < 100; i++) {
+  for (let i = 0; i < 100; i++) {
       func(...timeTestArr);
   }
   console.timeEnd(label);
